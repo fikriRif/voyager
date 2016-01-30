d7573a9c7daeb2e4dcb354ca90d86cea268741fc
Tony Lea <tnylea@gmail.com>
2016-01-30 15:31:34 -0800

    Adding more Table seeders

--- src/database/seeds/VoyagerDatabaseSeeder.php
+++ src/database/seeds/VoyagerDatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call('DataTypesTableSeeder');
         $this->call('DataRowsTableSeeder');
         $this->call('UsersTableSeeder');
+        $this->call('PostsTableSeeder');
     }
 }
